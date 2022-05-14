37b333787881dcf26328ff5fb9de50ccb0836f25
Dyanngx <Dyanngx@gmail.com>
2022-05-14 21:13:13 +0700

    Update URL & get Input Email element

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import time
   
 # Main Function
@@ -7,18 +8,26 @@ if __name__ == '__main__':
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
     options.add_argument('--log-level=3')
-  
+   
+
     # Provide the path of chromedriver present on your system.
-    driver = webdriver.Chrome("/Users/tuannd/Documents/GitHub/selenium-acs/selenium/webdriver/chrome/chromedriver2",
+    '''driver = webdriver.Chrome("/Users/tuannd/Documents/GitHub/selenium-acs/selenium/webdriver/chrome/chromedriver2",
                               chrome_options=options)
+    #config webdriver chrome manually
+    '''
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.set_window_size(1920,1080)
   
     # Send a get request to the url
-    driver.get('https://www.geeksforgeeks.org/')
-    btnLogin = driver.find_element_by_class_name('login-modal-btn')
-    btnLogin.click()
-    inputUser = driver.find_element_by_name('user')
-    inputPass = driver.find_elements_by_name('pass')
-    inputUser.get_attribute('value')
-    inputPass.get_attribute('value')
+    driver.get('https://accounts.shopify.com/lookup')
+    inputEmail = driver.find_element_by_class_name('next-input type-ahead-input email-typo-input')
+    inputEmail.get_attribute('value')
+    # btnLogin.click()
+   
+    # inputUser = driver.find_element_by_name('user')
+    # inputPass = driver.find_elements_by_name('pass')
+    # inputUser.get_attribute('value')
+    # inputPass.get_attribute('value')
+    # btnNext = driver.find_element_by_class_name('ui-button ui-button--primary ui-button--full-width ui-button--size-large')
+    # btnNext.click()
    
